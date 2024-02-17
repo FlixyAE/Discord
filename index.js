513b58f4a445d8e90a10a00200fd9400c86e6e06
FlixyAE <145846732+FlixyAE@users.noreply.github.com>
2024-02-17 21:32:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('ready', async () => {
     .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1164832441639379026/1207996512777404448/542E67BF-6022-463F-95FC-2D74ED6F5C33.gif?ex=65e1ad6c&is=65cf386c&hm=187b74e1e29b731b437c15ed275aa6caebb03d33118043849f3b0a292df52563&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('Forreal.') //Text when you hover the Large image
 .setAssetsSmallImage('https://cdn.discordapp.com/emojis/1173090926080577706.gif') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('verified.') //Text when you hover the Small image
+    .setAssetsSmallText('') //Text when you hover the Small image
     .addButton('Subscribe To My Youtube Channel', 'https://www.youtube.com/@FlixyAE/')
     .addButton('Join The Discord Server', 'https://discord.gg/obiwan');
 
