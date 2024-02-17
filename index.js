de5d88b421671593118b8fbad6258b58137463ef
FlixyAE <145846732+FlixyAE@users.noreply.github.com>
2024-02-17 14:05:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on('ready', async () => {
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1164832441639379026/1207996512777404448/542E67BF-6022-463F-95FC-2D74ED6F5C33.gif?ex=65e1ad6c&is=65cf386c&hm=187b74e1e29b731b437c15ed275aa6caebb03d33118043849f3b0a292df52563&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('My Time') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://cdn.discordapp.com/emojis/1173090926080577706.gif') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Insert Here') //Text when you hover the Small image
     .addButton('Join The Discord Server', 'https://discord.gg/obiwan')
     .addButton('Subscribe To My Youtube Channel', 'https://www.youtube.com/@FlixyAE');
