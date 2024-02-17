d283cd691069aaed99f0f465986ebf5deee3d960
FlixyAE <145846732+FlixyAE@users.noreply.github.com>
2024-02-17 21:50:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,17 +25,17 @@ client.on('ready', async () => {
   const r = new Discord.RichPresence()
     .setApplicationId('1208392776413220865')
     .setType('STREAMING')
-    .setURL('https://www.twitch.tv/flare_discord') //Must be a youtube video link 
-    .setState('Hosting')
-    .setName('Flare')
-    .setDetails(`Flare Discord Bot.`)
+    .setURL('https://www.twitch.tv/the_flixy15') //Must be a youtube video link 
+    .setState('Most Likely AFK')
+    .setName('Forreal')
+    .setDetails(`linktr.ee/flixy.ae.`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://media.discordapp.net/attachments/1139564276042965164/1139629624490475570/untitled15.png?width=426&height=426') //You can put links in tenor or discord and etc.
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1164832441639379026/1207996512777404448/542E67BF-6022-463F-95FC-2D74ED6F5C33.gif?ex=65e1ad6c&is=65cf386c&hm=187b74e1e29b731b437c15ed275aa6caebb03d33118043849f3b0a292df52563&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('Flare logo.') //Text when you hover the Large image
-.setAssetsSmallImage('https://media.discordapp.net/attachments/1139564276042965164/1139629624268181535/untitled14.png?width=426&height=426') //You can put links in tenor or discord and etc.
+.setAssetsSmallImage('https://cdn.discordapp.com/emojis/1173090926080577706.gif') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('verified.') //Text when you hover the Small image
-    .addButton('Invite Flare To Your Server', 'https://discord.com/api/oauth2/authorize?client_id=1136380915564687421&permissions=8&scope=applications.commands%20bot/')
-    .addButton('Github', 'https://github.com/Fady-GadE/');
+    .addButton('Join The Discord Server', 'https://discord.gg/obiwan')
+    .addButton('Sub To The YT Ig', 'https://youtube.com/@FlixyAE');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "online" }); //dnd, online, idle, offline
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `Flare Discord Bot.`;
+      const newDetails = `linktr.ee/flixy.ae.`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
