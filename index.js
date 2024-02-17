0ae49f1b57377f99db225ac6b744b9812026eb36
FlixyAE <145846732+FlixyAE@users.noreply.github.com>
2024-02-17 14:29:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Europe/Oslo', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -29,7 +29,7 @@ client.on('ready', async () => {
     .setState('Might Be Afk')
     .setName('mrnekrozyt')
     .setDetails(`linktr.ee/flixy.ae [${formatTime()}]`)
-    .setStartTimestamp(Date.now())
+    .setStartTimestamp(Date.Current())
  .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1164832441639379026/1207996512777404448/542E67BF-6022-463F-95FC-2D74ED6F5C33.gif?ex=65e1ad6c&is=65cf386c&hm=187b74e1e29b731b437c15ed275aa6caebb03d33118043849f3b0a292df52563&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('My Time') //Text when you hover the Large image
     .setAssetsSmallImage('https://cdn.discordapp.com/emojis/1173090926080577706.gif') //You can put links in tenor or discord and etc.
