56444347bb68fd92052d94fc298fa6b9df363759
FlixyAE <145846732+FlixyAE@users.noreply.github.com>
2024-02-27 10:24:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'Europe/Berlin', //https://www.zeitverschiebung.net/en/ and find your city and enter here
-    hour12: false,
+    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    hour12: true,
     hour: 'numeric',
     minute: 'numeric'
   };
@@ -23,19 +23,19 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('1208392776413220865')
+    .setApplicationId('1136410082255581245')
     .setType('STREAMING')
-    .setURL('https://www.twitch.tv/the_flixy15') //Must be a youtube video link 
-    .setState('Most Likely AFK')
-    .setName('Forreal')
-    .setDetails(`linktr.ee/flixy.ae.`)
+    .setURL('https://www.twitch.tv/flare_discord') //Must be a youtube video link 
+    .setState('Hosting')
+    .setName('Flare')
+    .setDetails(`Flare Discord Bot.`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1164832441639379026/1207996512777404448/542E67BF-6022-463F-95FC-2D74ED6F5C33.gif?ex=65e1ad6c&is=65cf386c&hm=187b74e1e29b731b437c15ed275aa6caebb03d33118043849f3b0a292df52563&') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('AFK') //Text when you hover the Large image
-.setAssetsSmallImage('https://cdn.discordapp.com/emojis/1173090926080577706.gif') //You can put links in tenor or discord and etc.
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/1139564276042965164/1139629624490475570/untitled15.png?width=426&height=426') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Flare logo.') //Text when you hover the Large image
+.setAssetsSmallImage('https://media.discordapp.net/attachments/1139564276042965164/1139629624268181535/untitled14.png?width=426&height=426') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('verified.') //Text when you hover the Small image
-    .addButton('Join The Debating Discord Server', 'https://discord.gg/skypiea')
-    .addButton('My YT Channel', 'https://youtube.com/@FlixyAE?si=RTvqZbwvT5E9Z3nI');
+    .addButton('Invite Flare To Your Server', 'https://discord.com/api/oauth2/authorize?client_id=1136380915564687421&permissions=8&scope=applications.commands%20bot/')
+    .addButton('Github', 'https://github.com/Fady-GadE/');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "online" }); //dnd, online, idle, offline
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `linktr.ee/flixy.ae.`;
+      const newDetails = `Flare Discord Bot.`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
