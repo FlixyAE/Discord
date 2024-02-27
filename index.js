b0c2fe3df99ac5e723dfd5f8a35fbb8ca8d1bec6
FlixyAE <145846732+FlixyAE@users.noreply.github.com>
2024-02-27 10:19:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'Europe/Oslo', //https://www.zeitverschiebung.net/en/ and find your city and enter here
-    hour12: true,
+    timeZone: 'Europe/Berlin', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    hour12: false,
     hour: 'numeric',
     minute: 'numeric'
   };
