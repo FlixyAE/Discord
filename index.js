9e5b18ecebd70d198a9c52e525b13fa7a655a184
FlixyAE <145846732+FlixyAE@users.noreply.github.com>
2024-02-27 10:40:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
     timeZone: 'Europe/Berlin', //https://www.zeitverschiebung.net/en/ and find your city and enter here
-    hour12: false,
+    hour12: true,
     hour: 'numeric',
     minute: 'numeric'
   };
@@ -31,7 +31,7 @@ client.on('ready', async () => {
     .setDetails(`linktr.ee/flixy.ae.`)
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1164832441639379026/1207996512777404448/542E67BF-6022-463F-95FC-2D74ED6F5C33.gif?ex=65e1ad6c&is=65cf386c&hm=187b74e1e29b731b437c15ed275aa6caebb03d33118043849f3b0a292df52563&') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('AFK') //Text when you hover the Large image
+    .setAssetsLargeText('AFK.') //Text when you hover the Large image
 .setAssetsSmallImage('https://cdn.discordapp.com/emojis/1173090926080577706.gif') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('verified.') //Text when you hover the Small image
     .addButton('Join The Debating Discord Server', 'https://discord.gg/skypiea')
