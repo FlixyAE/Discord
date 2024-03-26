f37683f76fbaae53c83e37598848077de86727b9
FlixyAE <145846732+FlixyAE@users.noreply.github.com>
2024-03-26 17:34:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,31 +25,31 @@ client.on('ready', async () => {
   const r = new Discord.RichPresence()
     .setApplicationId('1208392776413220865')
     .setType('STREAMING')
-    .setURL('https://www.twitch.tv/the_flixy15') //Must be a youtube video link 
-    .setState('Most Likely AFK')
-    .setName('Forreal')
-    .setDetails(`linktr.ee/flixy.ae.`)
+    .setURL('https://www.twitch.tv/flixy_14') //Must be a youtube video link 
+    .setState('AFK')
+    .setName('𝓕𝓛𝓘𝓧𝓨❦')
+    .setDetails(`linktr.ee/flixy.ae`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://i.pinimg.com/originals/b6/4f/7e/b64f7e4f813385e4de1ee3f30241e320.gif') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('AFK.') //Text when you hover the Large image
-.setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('𝓕𝓛𝓘𝓧𝓨❦.') //Text when you hover the Small image
-    .addButton('Join The Discord Server', 'https://discord.gg/frieza')
-    .addButton('My Links', 'https://linktr.ee/flixy.ae')
+    .setAssetsLargeImage('https://media1.giphy.com/media/I6wUi5eTdUCWI/giphy.gif') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Forreal.') //Text when you hover the Large image
+  .setAssetsSmallImage('https://media1.giphy.com/media/D6aoDE0OSYiYojubBW/giphy.gif?cid=6c09b952joz86vbpk26r0wnvpq9vbbb481l49inc8yz9veyl&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s) //You can put links in tenor or discord and etc.
+      .setAssetsSmallText('verified.') //Text when you hover the Small image
+      .addButton('My Links', 'https://linktr.ee/flixy.ae')
+      .addButton('gg/frieza', 'https://discord.gg/frieza');
 
-  client.user.setActivity(r);
-  client.user.setPresence({ status: "online" }); //dnd, online, idle, offline
+    client.user.setActivity(r);
+    client.user.setPresence({ status: "online" }); //dnd, online, idle, offline
 
-  let prevTime = null;
-  setInterval(() => {
-    const newTime = formatTime();
-    if (newTime !== prevTime) {
-      const newDetails = `linktr.ee/flixy.ae.`;
-      r.setDetails(newDetails);
-      client.user.setActivity(r);
-      prevTime = newTime;
-    }
-  }, 1000); // Update every second
-});
+    let prevTime = null;
+    setInterval(() => {
+      const newTime = formatTime();
+      if (newTime !== prevTime) {
+        const newDetails = `linktr.ee/flixy.ae`;
+        r.setDetails(newDetails);
+        client.user.setActivity(r);
+        prevTime = newTime;
+      }
+    }, 1000); // Update every second
+  });
 
-client.login(process.env['TOKEN']);
+  client.login(process.env['TOKEN']);
